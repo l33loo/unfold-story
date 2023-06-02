c52910a9da682f91e88c9cf6088034edae31aec4
Lila Karpowicz <lila@karpowicz.dev>
2023-06-02 15:38:34 +0000

    add tests for un/marshalling arbitrary json

---
+++ server_test.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"testing"
+)
+
+// Test different scenarios for passing
+// an arbitrary JSON from the browser
+// to the server
+
+func TestUnmarshalJsonStruct(t *testing.T) {
+	type User struct {
+		User string
+	}
+	type Obj struct {
+		Broadcast User
+	}
+	str := `{"Broadcast": {"User": "name"}}`
+	b := Obj{}
+	err := json.Unmarshal([]byte(str), &b)
+	if err != nil {
+		t.Fatalf(`JSON struct unmarshal error: "%s", should not have error`, err.Error())
+	}
+}
+
+func TestUnmarshalJsonStructString(t *testing.T) {
+	type User struct {
+		User string
+	}
+	type Obj struct {
+		Broadcast User
+	}
+	str := `{"Broadcast": "{\"User\": \"name\"}"}`
+	b := Obj{}
+	err := json.Unmarshal([]byte(str), &b)
+	log.Println(err.Error())
+	if err == nil {
+		t.Fatalf(`JSON struct unmarshal gives no error, there should be an error`)
+	}
+}
+
+func TestUnmarshalJsonString(t *testing.T) {
+	type Obj struct {
+		Broadcast string
+	}
+	str := `{"Broadcast": "{\"User\": \"name\"}"}`
+	b := Obj{}
+	err := json.Unmarshal([]byte(str), &b)
+	if err != nil {
+		t.Fatalf(`JSON string unmarshal error: "%s", should not have error`, err.Error())
+	}
+
+	type User struct {
+		User string
+	}
+	s := User{}
+	err = json.Unmarshal([]byte(b.Broadcast), &s)
+	if err != nil {
+		t.Fatalf(`JSON sub-struct unmarshal error: "%s", should not have error`, err.Error())
+	}
+}
+
+func TestUnmarshalArbitraryJson(t *testing.T) {
+	type Obj struct {
+		Broadcast map[string]interface{}
+	}
+	str := `{"Broadcast": {"User": "name"}}`
+	b := Obj{}
+	err := json.Unmarshal([]byte(str), &b)
+	if err != nil {
+		t.Fatalf(`JSON arbitrary struct unmarshal error: "%s", should not have error`, err.Error())
+	}
+
+	if b.Broadcast["User"] != "name" {
+		t.Fatalf(`JSON arbitrary struct unmarshal: got "%s", want "name"`, b.Broadcast["User"])
+	}
+}
+
+func TestMarshalArbitraryJson(t *testing.T) {
+	type Obj struct {
+		Broadcast map[string]interface{}
+	}
+	s := Obj{Broadcast: map[string]interface{}{
+		"User": "name",
+	}}
+	s.Broadcast["User"] = "name"
+	b, err := json.Marshal(s)
+	if err != nil {
+		t.Fatalf(`JSON arbitrary json marshal error: "%s", should not have error`, err.Error())
+	}
+
+	want := `{"Broadcast":{"User":"name"}}`
+	if string(b) != want {
+		t.Fatalf(`JSON arbitrary struct unmarshal: got "%s", want "%s"`, string(b), want)
+	}
+}
